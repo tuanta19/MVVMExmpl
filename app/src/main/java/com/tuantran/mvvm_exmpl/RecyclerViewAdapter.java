45a316ebcf3c996e356cd809e9386f9d2f8b0037
tuanta19 <techman2020@gmail.com>
2019-10-07 17:31:49 +0700

    complete

--- app/src/main/java/com/tuantran/mvvm_exmpl/RecyclerViewAdapter.java
+++ app/src/main/java/com/tuantran/mvvm_exmpl/RecyclerViewAdapter.java
@@ -1,15 +1,70 @@
 package com.tuantran.mvvm_exmpl;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class RecyclerViewAdapter  {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>  {
     private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames=new ArrayList<>();
+    private ArrayList<String> mImages=new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> imageNames, ArrayList<String> images) {
+        mImageNames = imageNames;
+        mImages = images;
+        mContext = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        ViewHolder holder=new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(i))
+                .into(viewHolder.image);
+
+        viewHolder.imageName.setText(mImageNames.get(i));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: "+mImageNames.get(i));
+
+                Toast.makeText(mContext, mImageNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
